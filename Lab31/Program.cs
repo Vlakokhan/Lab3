597f581f06a94d1f05b22a10ee8cf41e5a3e9638
vladislav27ko@gmail.com <vladislav27ko@gmail.com>
2020-06-10 21:19:33 +0300

    11

---
+++ Lab31/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Lab31
+{
+       class Program
+       {
+           static void Main(string[] args)
+           {
+               string s = Console.ReadLine();
+               int bestLength = int.MaxValue;
+               List<string> bestWords = new List<string>();
+               foreach (string word in s.Split(' '))
+               {
+                   if (word.Length == bestLength)
+                   {
+                       bestWords.Add(word);
+                   }
+                   else if (word.Length < bestLength)
+                   {
+                       bestLength = word.Length;
+                       bestWords = new List<string>() { word };
+                   }
+               }
+   
+               foreach (string word in bestWords)
+               {
+                   Console.WriteLine(word);
+               }
+   
+               Console.ReadKey();
+           }
+       }
+   }
