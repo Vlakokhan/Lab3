d70389037adb14a065ca805ce9e6b05e9f76d9fc
vladislav27ko@gmail.com <vladislav27ko@gmail.com>
2020-06-10 22:24:34 +0300

    11A

---
+++ Lab 31A/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab_31A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int k = 0;
+            foreach (var variable in s)
+            {
+                if (char.IsUpper(variable))
+                {
+                    k++;
+                }
+            }
+            Console.WriteLine("Кiлькiсть великих лiтер: "+k);
+            Console.ReadKey();
+        }
+    }
+}
