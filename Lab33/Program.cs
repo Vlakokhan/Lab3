ebe9ba7cf80808a721359de5bc93a4f046c791ba
vladislav27ko@gmail.com <vladislav27ko@gmail.com>
2020-06-12 16:18:15 +0300

    33

---
+++ Lab33/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText("C:\Users\vladi\RiderProjects\Lab33\input.txt");
+            foreach (string s in "0 1 2 3 4 5 6 7 8 9".Split(' '))
+            {
+                text = text.Replace(s, "");
+            }
+            System.IO.File.WriteAllText("C:\Users\vladi\RiderProjects\Lab33\output.txt", text);
+
+            Console.ReadKey();
+        }
+    }
+}
